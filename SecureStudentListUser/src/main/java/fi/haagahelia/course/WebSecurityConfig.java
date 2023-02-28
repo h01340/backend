0195982dda3b30e4b366796e2580e333c931b6d9
Pellikka Minna <h01340@haaga-helia.fi>
2023-02-28 18:30:40 +0200

    Spring Boot ver 3.0 corrections

--- SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -1,38 +1,42 @@
 package fi.haagahelia.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import fi.haagahelia.course.web.UserDetailServiceImpl;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
     @Autowired
     private UserDetailServiceImpl userDetailsService;	
 	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
-        .and()
-        .authorizeRequests()
-          .anyRequest().authenticated()
-          .and()
-      .formLogin()
-          .defaultSuccessUrl("/studentlist", true)
-          .permitAll()
-          .and()
-      .logout()
-          .permitAll();
+    @Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests().requestMatchers("/css/**").permitAll()
+		.and()
+		.authorizeHttpRequests().anyRequest().authenticated()
+		//for h2 console
+		.and()
+		.headers().frameOptions().disable()			
+		.and()
+		.formLogin().defaultSuccessUrl("/studentlist", true)
+		.and()
+		.logout().permitAll();
+		
+		http.cors().and().csrf().disable();
+		
+		//OHjelmistoprojekti1:n matskuun tämä
+		//.addFilterAfter(new CsrfLoggerFilter(), CsrfFilter.class); 
+		
+		return http.build();
     }
     
     @Autowired
---
+++ SecureStudentListUser/src/test/java/fi/haagahelia/course/StudentListSecuredApplicationTests.java
@@ -0,0 +1,14 @@
+package fi.haagahelia.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class StudentListSecuredApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
