0fe8c72fa49b276f110f2877758ae64b6c09109e
Pellikka Minna <h01340@haaga-helia.fi>
2023-02-28 18:11:37 +0200

    just security dependency needed in this demo

--- SecurityDemo/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecurityDemo/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -1,54 +1,44 @@
 package fi.haagahelia.course;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  {
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .requestMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
 
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests().requestMatchers("/", "/home").permitAll()
+		.and()
+			.authorizeHttpRequests().anyRequest().authenticated()
+		.and()
+			.formLogin()
+			.loginPage("/login")
+			.permitAll()
+		.and()
+			.logout().permitAll();
+				
+		return http.build();
+	}
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList();
-    	UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
-
-    	users.add(user);
-    	
-    	user = User.withDefaultPasswordEncoder()
-                   .username("admin")
-                   .password("admin")
-                   .roles("USER", "ADMIN")
-                   .build();
-    	
-    	users.add(user);
-    	
-        return new InMemoryUserDetailsManager(users);
-    }
+	/*
+	 * protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	 * { auth.inMemoryAuthentication() .passwordEncoder(passwordEncoder())
+	 * .withUser("user") .password(passwordEncoder().encode("userPass"))
+	 * .roles("USER");
+	 * 
+	 * auth.inMemoryAuthentication() .passwordEncoder(passwordEncoder())
+	 * .withUser("admin") .password(passwordEncoder().encode("adminPass"))
+	 * .roles("USER", "ADMIN"); }
+	 * 
+	 * @Bean public PasswordEncoder passwordEncoder() { return new
+	 * BCryptPasswordEncoder(); }
+	 */
+  
+  
 }
